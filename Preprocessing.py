f5aa8064e6026c035bfa1ff5509ad076d1689a52
Fatini Nadhirah <50139706+FatiniNadhirah5@users.noreply.github.com>
2019-09-12 13:34:16 +0800

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,3 +1,4 @@
+#list of library
 import PyPDF2  #pdf library
 import nltk
 from nltk.corpus import stopwords 
@@ -5,6 +6,8 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import string
 from nltk.stem import WordNetLemmatizer 
+from matplotlib import pyplot
+import regex as re
 #--------------------------------------------------------------------#
 # creating a pdf file object 
 pdfFileObj = open('combined_example.pdf', 'rb') 
@@ -34,36 +37,38 @@ while count < num_pages:
     pageObj = pdfReader.getPage(count)
     count +=1
     text += pageObj.extractText()
-    f = text.replace("\n", " ") 
 print("---------------------------------------------------------------------")
 
 # -------------------------------------------------------------------------------------
 # Tokenizations
 print("Tokenizations")
-tokenization_words = word_tokenize(f)
-
+tokenization_words = word_tokenize(text)
 print(tokenization_words)
 
-
 # -------------------------------------------------------------------------------------
+#normalization
 # Removed punctuation
 punctuation_words = [word for word in tokenization_words if word.isalpha()]
-print(punctuation_words)
 
 # convert to lower case
-normalization = [w.lower() for w in punctuation_words]
-print("---------------------------------------------------------------------")
+lower_case = [w.lower() for w in punctuation_words]
 
+# removes spaces
+remove_spaces = [w.strip() for w in lower_case]
+
+# removes number
+remove_num = [''.join(x for x in w if x.isalpha()) for w in remove_spaces] 
 # -------------------------------------------------------------------------------------
 print("---------------------------------------------------------------------")
+
 print("Stopwords")
 stop_words = set(stopwords.words('english'))
-result = [word for word in normalization if not word in stop_words]
+result = [word for word in remove_num if not word in stop_words]
 print(result)
+
 print("---------------------------------------------------------------------")
 # -------------------------------------------------------------------------------------
 print("POS Tag")
-
 tag = nltk.pos_tag(result)
 print(tag)
 
@@ -74,26 +79,24 @@ print("Stemming")
 porter = PorterStemmer()
 stemmed = [porter.stem(word) for word in result]
 print(stemmed)
-print("---------------------------------------------------------------------")
 
+print("---------------------------------------------------------------------")
 # -------------------------------------------------------------------------------------
 #Lemmmatization
 print("Lemmatization")
 wordnet_lemmatizer = WordNetLemmatizer()
 lemmatize = [wordnet_lemmatizer.lemmatize(word) for word in result]
 print(lemmatize)
+
 print("---------------------------------------------------------------------")
 # -------------------------------------------------------------------------------------
 
 #write the contents in text file 
-with open("Stemmed.txt", "w") as text_file:
-    text_file.write("{0}".format(stemmed).encode('utf8'))
-
 with open("Tokenization.txt", "w") as text_file:
-    text_file.write("{0}".format(result).encode('utf8'))
+    text_file.write("{0}".format(tokenization_words).encode('utf8'))
 
 with open("Stopwords.txt", "w") as text_file:
-    text_file.write("{0}".format(normalization).encode('utf8'))
+    text_file.write("{0}".format(result).encode('utf8'))
 
 with open("Tag.txt", "w") as text_file:
     text_file.write("{0}".format(tag).encode('utf8'))
@@ -101,8 +104,10 @@ with open("Tag.txt", "w") as text_file:
 with open("Lemmatization.txt", "w") as text_file:
     text_file.write("{0}".format(lemmatize).encode('utf8'))
 
+with open("Stemmed.txt", "w") as text_file:
+    text_file.write("{0}".format(stemmed).encode('utf8'))
+
 print("successfully saved!")
 
 print("---------------------------------------------------------------------")
-
 # -------------------------------------------------------------------------------------
